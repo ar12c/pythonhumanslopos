e98b512ff6042c0866582e7e97d825e6ae46bf7e
ar12c <okemothebiggestmeower@gmail.com>
2026-07-10 21:36:38 +0800

    ver

--- DOS.py
+++ DOS.py
@@ -3,6 +3,7 @@ import os
 import pathlib 
 date = datetime.datetime.now()
 version = "0.1 alpha"
+dos = "OkemoDOS"
 
 print("Labs21 OKEMO-DOS version",version,"\nOSS 2026 OkemoWare \n⚠ fiddled with bugs, proceed at your own caution") # top 10 okemoware
 print("\nLocal time is",date.strftime("%c"))
@@ -15,9 +16,15 @@ def exit():
     print("Shutting Down OkemoDOS")
     quit()
 
+def ver():
+    print("""\\\\  //   Version""",version)
+    print(""">>  <<  """,dos)
+    print("""//  \\\\""")
+
 Commands = {
     "help": help,
     "exit": exit,
+    "ver": ver,
 }
 
 ## While true loop
